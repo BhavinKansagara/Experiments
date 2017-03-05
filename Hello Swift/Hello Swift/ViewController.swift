4131537088cca5c20e4d57cad83eeaa696ec1a6c
Bhavin Kansagara <bhavin.ios.msg@gmail.com>
2017-03-05 16:38:34 +0530

    First commit with the UIAlertController with style alert

---
+++ Hello Swift/Hello Swift/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Hello Swift
+//
+//  Created by Bhavin Kansagara on 05/03/17.
+//  Copyright © 2017 Bhavin Kansagara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib
+    }
+    
+    //MARK: - Button Actions
+    @IBAction func btnShowAlertPressed(sender: UIButton){
+        
+        let title = "Hello Swift"
+        let message = "This is just the welcome message for you. Explore more on swift"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let actionTitle = "OK"
+        let action = UIAlertAction(title: actionTitle, style: .default) { (k) in
+            print("ok presed")
+        }
+        
+        alertController.addAction(action)
+        alertController.show(self, sender: self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
